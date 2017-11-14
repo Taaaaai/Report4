2397a9d6f3621ba1f5e91fe33055b9fbe0067bd2
Taisei Arakaki <e175746@ie.u-ryukyu.ac.jp>
2017-11-15 00:15:33 +0900

    final change

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args){
-        String str;
+
         try{
-            str = null;
-            System.out.println(str.length());
-            }catch (NullPointerException e){
+            String str = "3．14";
+            double value = Double.parseDouble(str);
+        }catch (NumberFormatException e){
             System.out.println("例外が発生しました。");
             System.out.println(e.getMessage());
         }
